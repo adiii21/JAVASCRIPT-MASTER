c0781197094f1d24dc5f85630dae17c4f0c0f0b5
Aditya <aditya21official@gmail.com>
2022-10-22 14:38:57 +0530

    basics

---
+++ functions.js
@@ -0,0 +1,18 @@
+let myPoints = 3
+
+
+function addPoints(){
+    myPoints += 3
+}
+
+function removePoints(){
+    myPoints -= 1
+}
+
+addPoints()
+addPoints()
+addPoints()
+removePoints() 
+removePoints() 
+
+console.log(myPoints)
---
+++ swap.js
@@ -0,0 +1,7 @@
+
+var a = "3";
+var b = "8";
+
+
+console.log("a is"+a);
+console.log("b is"+b);
