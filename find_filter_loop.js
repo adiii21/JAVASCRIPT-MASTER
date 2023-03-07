74d02eb3258a6fadf146a3f4a656033ffd845b4b
Aditya Singh <85675018+adiii21@users.noreply.github.com>
2023-03-07 11:03:37 +0530

    find, filter, loop

---
+++ find_filter_loop.js
@@ -0,0 +1,182 @@
+// const students = [
+//     {
+//       id: "11ce",
+//       first_name: "Dall",
+//       last_name: "Puckring",
+//     },
+//     {
+//       id: "2958",
+//       first_name: "Margarete",
+//       last_name: "Brandi",
+//     },
+//     {
+//       id: "565a",
+//       first_name: "Bendicty",
+//       last_name: "Woodage",
+//     },
+//     {
+//       id: "3a16",
+//       first_name: "Micki",
+//       last_name: "Mattes",
+//     },
+//     {
+//       id: "f396",
+//       first_name: "Flory",
+//       last_name: "Gladeche",
+//     },
+//     {
+//       id: "de5f",
+//       first_name: "Jamill",
+//       last_name: "Emilien",
+//     },
+//     {
+//       id: "54cb",
+//       first_name: "Brett",
+//       last_name: "Aizikowitz",
+//     },
+//     {
+//       id: "9135",
+//       first_name: "Lorry",
+//       last_name: "Smallman",
+//     },
+//     {
+//       id: "978f",
+//       first_name: "Gilly",
+//       last_name: "Flott",
+//     },
+//   ];
+
+// // find() in JS 
+
+//   const student = students.find(stud => stud.id === '565a' );
+//   console.log(student);
+
+// Filter and find in JS 
+
+// const ages = [2,4,3,8];
+// function checkAdult(age){
+//     return age%2 !== 0;
+// }
+
+// console.log(ages.filter(checkAdult));
+
+// // map in js 
+// const faces = ['😃', '🤠', '🤡', '🤑', '😵', '🌞', '🐶', '😺'];
+
+// function addArms(face) {
+//     return `👋${face}👋`
+// }
+
+// const toys =faces.map(addArms);
+// console.log(toys);
+
+// const fullNames = ['Aditya','singh','poppy'].map(name => `${name}`);
+// console.log(fullNames);
+
+// function capitalize(str) {
+//     return str[0].toUpperCase()+ str.slice(1)+ ' Bays';
+// }
+
+// const fullNames = ['aditya','singh','poppy'].map(capitalize);
+// console.log(fullNames);
+
+// const orderTotals = [342,1002,523,34,634,854,1644,2222];
+
+// const orderTotalWithTax = orderTotals.map(total => 1);
+// console.log(orderTotalWithTax);
+
+// const students = [
+//     {
+//       id: "11ce",
+//       first_name: "Dall",
+//       last_name: "Puckring",
+//     },
+//     {
+//       id: "2958",
+//       first_name: "Margarete",
+//       last_name: "Brandi",
+//     },
+//     {
+//       id: "565a",
+//       first_name: "Bendicty",
+//       last_name: "Woodage",
+//     },
+//     {
+//       id: "3a16",
+//       first_name: "Micki",
+//       last_name: "Mattes",
+//     },
+//     {
+//       id: "f396",
+//       first_name: "Flory",
+//       last_name: "Gladeche",
+//     },
+//     {
+//       id: "de5f",
+//       first_name: "Jamill",
+//       last_name: "Emilien",
+//     },
+//     {
+//       id: "54cb",
+//       first_name: "Brett",
+//       last_name: "Aizikowitz",
+//     },
+//     {
+//       id: "9135",
+//       first_name: "Lorry",
+//       last_name: "Smallman",
+//     },
+//     {
+//       id: "978f",
+//       first_name: "Gilly",
+//       last_name: "Flott",
+//     },
+//   ];
+
+
+//   function findById(id) {
+//     return function isStudent(student){
+//         return student.id === id;
+//     }
+//   };
+
+//   const student = students.find(findById('565a'));
+
+//   console.log(student);
+
+// const people = [
+//     {
+//       birthday: "April 22, 1993",
+//       names: {
+//         first: "Keith",
+//         last: "Buckley",
+//       },
+//     },
+//     {
+//       birthday: "January 3, 1975",
+//       names: {
+//         first: "Larry",
+//         last: "Heep",
+//       },p(function(person) {
+//     const birthday = new Date(person.birthday);
+//     console.dir(birthday);
+//   })
+//   console.table(cleanPeople);
+
+//     },
+//     {
+//       birthday: "February 12, 1944",
+//       names: {
+//         first: "Linda",
+//         last: "Bermeer",
+//       },
+//     },
+//   ];
+
+//   const cleanPeople = people.ma
+
+// for loop 
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);    
+// }
