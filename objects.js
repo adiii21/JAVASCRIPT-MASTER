7afe64fb831dc2ea164078d1777035f0494223ad
Aditya Singh <85675018+adiii21@users.noreply.github.com>
2023-03-10 10:46:30 +0530

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,65 @@
+// for of Loop 
+// const name = 'Wes Bos';
+// for(const letter of name){
+//     console.log(letter);
+// }
+
+// const numbers = [5, 54, 23, 98, 21, 39, 87,77];
+
+// for(const number of numbers){
+//     console.log(number);
+// }
+
+// const wes = {
+//     name: 'wes',
+//     age: 100,
+//     cool: true
+// }
+
+// for(const prop in wes){
+//     console.log(prop);
+// }
+
+// const baseHumanStats = {
+//     feet: 2,
+//     arms: 2,
+//     eyes: 2,
+//     head: 1
+// };
+
+// function Human(name){
+//     this.name = name;
+// }
+// Human.prototype = baseHumanStats;
+// const wes2 = new Human('wes');
+
+// while loop 
+
+// let cool = true;
+// while(cool === true){
+    //     console.log('You are cool');
+    // }
+    
+    // do while loop 
+    
+//     let cool = true;
+// do {
+    
+//         console.log('You are cool');
+// }while(cool === false);
+
+// objects in JS 
+
+// const person = {
+//     name: 'John',
+//     age: 100,
+//     clothing: {
+//         shirts: 10,
+//         pants: 2,
+//     }
+// };
+
+// person.job = 'Web Developer';
+// person.age = 50;
+
+// console.log(person);
