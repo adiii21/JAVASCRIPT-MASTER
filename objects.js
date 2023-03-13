3f5dc00f4c28af3a5be2e0bd8bbfc57707ad07b4
Aditya Singh <85675018+adiii21@users.noreply.github.com>
2023-03-13 19:03:01 +0530

    Update objects.js

--- objects.js
+++ objects.js
@@ -93,3 +93,60 @@
 // console.log(name);
 // Delete from the object
 // console.log(delete wes.name);
+
+
+// const currencies = {
+//   USD: "United States Dollar",
+//   AUD: "Australian Dollar",
+//   BGN: "Bulgarian Lev",
+//   BRL: "Brazilian Real",
+//   CAD: "Canadian Dollar",
+//   CHF: "Swiss Franc",
+//   CNY: "Chinese Yuan",
+//   CZK: "Czech Republic Koruna",
+//   DKK: "Danish Krone",
+//   GBP: "British Pound Sterling",
+//   HKD: "Hong Kong Dollar",
+//   HRK: "Croatian Kuna",
+//   HUF: "Hungarian Forint",
+//   IDR: "Indonesian Rupiah",
+//   ILS: "Israeli New Sheqel",
+//   INR: "Indian Rupee",
+//   JPY: "Japanese Yen",
+//   KRW: "South Korean Won",
+//   MXN: "Mexican Peso",
+//   MYR: "Malaysian Ringgit",
+//   NOK: "Norwegian Krone",
+//   NZD: "New Zealand Dollar",
+//   PHP: "Philippine Peso",
+//   PLN: "Polish Zloty",
+//   RON: "Romanian Leu",
+//   RUB: "Russian Ruble",
+//   SEK: "Swedish Krona",
+//   SGD: "Singapore Dollar",
+//   THB: "Thai Baht",
+//   TRY: "Turkish Lira",
+//   ZAR: "South African Rand",
+//   EUR: "Euro",
+// };
+
+// console.log(currencies);
+
+// const optionHTML = generateOptions(currencies);
+// console.log(optionHTML);
+
+// function generateOptions(options) {
+//   return Object.entries(options).map(([currencyCode, currencyName]) => {
+//     console.log(currencyCode, currencyName);
+//   });
+// }
+
+// const name = [1,2,4,2,8,5,745,42];
+
+// for(const letter of name){
+//     console.log(letter);
+// }
+
+// for(const letter of name){
+//     console.log(letter);
+// }
