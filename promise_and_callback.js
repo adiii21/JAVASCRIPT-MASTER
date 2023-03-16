c446079c600fc349fbcbd79c011a962a33b63bdf
Aditya Singh <85675018+adiii21@users.noreply.github.com>
2023-03-16 15:08:27 +0530

    Update promise_and_callback.js

--- promise_and_callback.js
+++ promise_and_callback.js
@@ -138,3 +138,58 @@
 // .then(function(paymentInfo){
 //     return updateWalletBalance(paymentInfo);
 // });
+
+
+
+// Promise chain and Error handling
+
+// const cart =["shoes", "pants", "kurta"];
+
+// createOrder(cart)
+// .then(function(orderId){
+//     console.log(orderId)
+//     return orderId;
+// })
+// .then(function(orderId){
+//    return proceedToPayment(orderId);
+// })
+// .then(function(paymentInfo){
+//     console.log(paymentInfo)
+// })
+// .catch(function(err){
+//     console.log(err.message);
+// })
+
+
+// Produce End 
+
+// function createOrder(cart) {
+//     const pr = new Promise(function(resolve,reject){
+//         // createOrder
+//         // validatecart
+//         // orderId
+//         if(!validateCart(cart)){
+//             const err = new Error("Cart is not valid");
+//             reject(err);
+//         }
+//         const orderId = "12345";
+//         if(orderId){
+//             setTimeout(function(){
+//                 resolve(orderId);
+//             },5000)
+//         }
+//     });
+
+//     return pr;
+// }
+
+
+// function proceedToPayment(orderId){
+//     return new Promise((resolve,reject)=>{
+//         resolve("Payment Successful")
+//     })
+// }
+
+// function validateCart(){
+//     return true;
+// }
