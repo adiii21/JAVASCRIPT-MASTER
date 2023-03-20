5e218f24589679e6234517ea38de53eb3c1e6aad
Aditya Singh <85675018+adiii21@users.noreply.github.com>
2023-03-20 15:41:51 +0530

    Click Event Handling

---
+++ EventHandling.js
@@ -0,0 +1,57 @@
+// const butts = document.querySelector('.butts');
+// const coolbutts = document.querySelector('.coolbutts');
+
+// butts.removeEventListener('click', handleClick);
+
+// function handleClick(){
+//     console.log("Hehe good!");
+// }
+
+// coolbutts.addEventListener('/click', handleClick);
+// coolbutts.removeEventListener('click', handleClick);
+
+
+// function handleClick(){
+//     console.log("Hehe good!");
+// }
+
+// Arrow function 
+// const hooray = () => console.log("Hehe good!");
+
+// coolbutts.addEventListener('click', hooray);
+
+// const buyButtons = document.querySelectorAll("button.buy");
+
+
+// buyButtons.addEventListener('click',butItem);
+
+// console.dir(butts); It basically shows all the props that are available
+
+// forEach 
+
+
+// buyButtons.forEach(function(buyButton){
+//     // console.log('Binding the buy button');
+//     // buyButton.addEventListener('click',buyItem);
+//     console.log(buyButton);
+// });
+
+
+// function buyItem(){
+//     console.log("buyButtons");
+// }
+
+// Now moving the function outside 
+
+// function handleBuyButtonClick(buyButton) {
+//     console.log('Binding the buy button');
+//     buyButton.addEventListener('click', buyItem);
+// }
+
+// buyButtons.forEach(handleBuyButtonClick);
+
+// buyButtons.forEach((button)=>{
+//     button.addEventListener('click', ()=> {
+//         console.log("You clicked it!")
+//     })
+// })
