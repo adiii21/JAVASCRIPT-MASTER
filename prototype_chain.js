b208b139a31ad6622c594c7e34036791f4069a30
Aditya Singh <85675018+adiii21@users.noreply.github.com>
2023-03-22 10:44:48 +0530

    prototype chain

---
+++ prototype_chain.js
@@ -0,0 +1,18 @@
+// prototype chain 
+// function userCreator(name, score){
+//     const newUser = Object.create(userFunctionStore);
+//     newUser.name = name;
+//     newUser.score = score;
+//     return newUser;
+// };
+
+// const userFunctionStore = {
+//     increment: function(){this.score++;},
+//     login: function(){console.log('Logged in');}
+// };
+
+// const user1 = userCreator('will', 3);
+// const user2 = userCreator('Tim', 5);
+// user1.increment();
+// console.log(user1.increment);
+
