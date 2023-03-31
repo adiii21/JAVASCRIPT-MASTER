5866cdb402c196ae0c83e027270ec1d9cf2bb1a5
Aditya Singh <85675018+adiii21@users.noreply.github.com>
2023-03-31 11:48:38 +0530

    form validation

--- FormValidation.js
+++ FormValidation.js
@@ -1,1 +1,40 @@
 
+function validateform(){  
+    var name=document.myform.name.value;  
+    var firstpassword=document.myform.password.value;  
+    var secondpassword=document.myform.password2.value;
+    var num = document.myform.num.value;  
+    var email = document.myform.email.value;  
+    var atPosition =  email.indexOf("@");
+    var dotPosition =  email.lastIndexOf(".");
+    
+    if (name==null || name==""){  
+      alert("Name can't be blank");  
+      return false;  
+    }else if(firstpassword.length<6 || secondpassword.length<6){  
+      alert("Password must be at least 6 characters long.");  
+      return false;  
+    } else if(firstpassword !== secondpassword){
+        alert("Both the password are different."); 
+        return false; 
+    } else if(isNaN(num)) {
+        document.getElementById("numloc").innerHTML = "Enter Numeric value only";
+        return false; 
+    } else if(num.length!==10){
+        document.getElementById("numloc").innerHTML = "number should be atleast 10";
+        return false
+    } 
+    else if(atPosition<1 || dotPosition< atposition+2 || dotPosition+2>=email.length){
+        console.log(atPosition);
+        console.log(dotPosition);
+        document.getElementById("emailerr").innerHTML = "Enter correct email address";
+        return false;
+    } 
+    else{
+        alert("Login success");
+ 
+    };
+} 
+
+
+
